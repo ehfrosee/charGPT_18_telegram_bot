9c254acdc38da3767372051bf46afbaf7d5c9288
ehfrosee <ehfros.e.e@gmail.com>
2024-12-14 11:42:50 +0300

    Настроены библиотеки в requirements. Работает API. Работает отправка запросов и получение ответов от ChatGPT Проверка функциональности в test.py

--- chunks.py
+++ chunks.py
@@ -1,29 +1,32 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.docstore.document import Document
-from dotenv import load_dotenv
 import openai
 import os
 import re
 import requests
 
-# получим переменные окружения из .env
-load_dotenv()
 
 # API-key
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY_2")
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY_2")
 
 # задаем system
 default_system = "Ты-консультант в компании Simble, ответь на вопрос клиента на основе документа с информацией. Не придумывай ничего от себя, отвечай максимально по документу. Не упоминай Документ с информацией для ответа клиенту. Клиент ничего не должен знать про Документ с информацией для ответа клиенту"
 
-data_url = 'https://docs.google.com/document/d/11MU3SnVbwL_rM-5fIC14Lc3XnbAV4rY1Zd_kpcMuH4Y'
+#data_url = 'https://docs.google.com/document/d/11MU3SnVbwL_rM-5fIC14Lc3XnbAV4rY1Zd_kpcMuH4Y'
+data_url = 'data.txt'
 default_system_url = 'https://docs.google.com/document/d/1mwdXqx50imzvtPPbp7l8BTf3JsrUYQ0g2NhAwsjssJ8/edit?usp=sharing'
+
+
 class Chunk():
 
     def load_document_text(self, url: str) -> str:
         # Extract the document ID from the URL
+        # загружаем базу
         match_ = re.search('/document/d/([a-zA-Z0-9-_]+)', url)
+#        match_ = re.search(r'/document/d/([^/]+)/', url)
         if match_ is None:
             raise ValueError('Invalid Google Docs URL')
         doc_id = match_.group(1)
@@ -38,22 +41,21 @@ class Chunk():
     def __init__(self, path_to_base: str, sep: str = " ", ch_size: int = 1024, ch_overlap: int = 128):
 
         # загружаем базу
-        # with open(path_to_base, 'r', encoding='utf-8') as file:
-        #     document = file.read()
-        data_from_url = self.load_document_text(path_to_base)
+        print('загружаем базу')
+        with open(path_to_base, 'r', encoding='utf-8') as file:
+            document = file.read()
+#        document = self.load_document_text(path_to_base)
         # создаем список чанков
+#        print('создаем список чанков')
         source_chunks = []
-        # splitter = CharacterTextSplitter(separator=sep, chunk_size=ch_size)
-        # for chunk in splitter.split_text(document):
-        #     source_chunks.append(Document(page_content=chunk, metadata={}))
-
-        splitter = RecursiveCharacterTextSplitter(chunk_size=ch_size, chunk_overlap=ch_overlap,
-                                                  length_function=len)  # заполните нужными значениями, проведите эксперименты
-        for chunk in splitter.split_text(data_from_url):
+        splitter = CharacterTextSplitter(separator=sep, chunk_size=ch_size)
+        for chunk in splitter.split_text(document):
             source_chunks.append(Document(page_content=chunk, metadata={}))
 
         # создаем индексную базу
+#        print('создаем embeddings')
         embeddings = OpenAIEmbeddings()
+#        print(f'создаем индексную базу из {len(source_chunks)} элементов')
         self.db = FAISS.from_documents(source_chunks, embeddings)
 
     def get_answer(self, system_url: str = default_system_url, query: str = None):
@@ -76,3 +78,10 @@ class Chunk():
 
         return completion.choices[0].message.content
 
+
+if __name__ == "__main__":
+    # инициализация индексной базы
+    chunk = Chunk(path_to_base=data_url)
+    topic = "Как определяется сумма страховых выплат?"
+    answer = chunk.get_answer(query=topic)
+    print(answer)
--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from fastapi import FastAPI
 from chunks import Chunk
-import openai
 from pydantic import BaseModel
-from dotenv import load_dotenv
-
-#uvicorn main:app --port 5000
+from langchain_core.pydantic_v1 import BaseModel
+# uvicorn main:app --port 5000
 
 # инициализация индексной базы
-data_url = 'https://docs.google.com/document/d/11MU3SnVbwL_rM-5fIC14Lc3XnbAV4rY1Zd_kpcMuH4Y'
+data_url = 'data.txt'
 chunk = Chunk(path_to_base=data_url)
 
-# класс с типами данных параметров 
-class Item(BaseModel): 
+
+# класс с типами данных параметров
+class Item(BaseModel):
     text: str
+
 
 # создаем объект приложения
 app = FastAPI()
 
-# функция обработки get запроса + декоратор 
+
+# функция обработки get запроса + декоратор
 @app.get("/")
 def read_root():
     return {"message": "answer"}
 
-# функция обработки post запроса + декоратор 
+
+# функция обработки post запроса + декоратор
 @app.post("/api/get_answer")
 def get_answer(question: Item):
+    pass
     answer = chunk.get_answer(query=question.text)
     return {"message": answer}
-
